486c8c27fb7059a57aa1b575eb56d41cfcffdad0
yasainet <takumi.mizoguchi@gmail.com>
2023-10-29 09:01:06 +0900

    add time

--- scripts/txt2mask.py
+++ scripts/txt2mask.py
@@ -135,6 +135,9 @@ class Script(scripts.Script):
 				transforms.Resize((512, 512)),
 			])
 			img = transform(p.init_images[0]).unsqueeze(0)
+            # RGBA to RGB conversion
+            if img.shape[1] == 4;
+                img = img[:, :3, :, :]
 
 			prompts = mask_prompt.split(delimiter_string)
 			prompt_parts = len(prompts)
